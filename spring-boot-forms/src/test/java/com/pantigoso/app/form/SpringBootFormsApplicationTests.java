758f88e2decf9599297699070958e0ba9a48b797
josmig <pantigosomiguel59@gmail.com>
2020-07-06 17:40:33 -0500

    repo ejemplos y demos con springboot

---
+++ spring-boot-forms/src/test/java/com/pantigoso/app/form/SpringBootFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pantigoso.app.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
