d64fb35d4b5c96595a0757f5fd9c30745a2a3bec
josmig <pantigosomiguel59@gmail.com>
2020-07-11 22:43:42 -0500

    Formulario simple - Mapeo formulario & objeto model + valid (bindingresult)

--- spring-boot-forms/src/main/java/com/pantigoso/app/form/Controller/FormController.java
+++ spring-boot-forms/src/main/java/com/pantigoso/app/form/Controller/FormController.java
@@ -1,9 +1,16 @@
 package com.pantigoso.app.form.Controller;
 
+import com.pantigoso.app.form.Model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -12,11 +19,28 @@ public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("title","Formulario usuario");
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesoFormulario(Model model) {
+	public String procesoFormulario(@Valid  Usuario usuario, BindingResult result, Model model) {
+		model.addAttribute("title","Resultado del formulario");
+		//binding es cuando falla la validacion para saber eso
+		if(result.hasErrors()){
+			//obteniendo el mensaje de error
+			Map<String ,String> errores = new HashMap<>();
+
+			//Forma manual de obtener los errores de BindingResult
+				//getFiledErrors nos crea un arrelgo y lo recorremos
+			result.getFieldErrors().forEach(err ->{
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error",errores);
+			return "form";
+		}
+
+		model.addAttribute("user",usuario);
 		return "resultado";		
 	}
 	
